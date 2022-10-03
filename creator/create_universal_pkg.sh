733fa277b25cb295ac4749bbe9ca24eadf78e53d
Vikram Karve <85881329+vkarve-chef@users.noreply.github.com>
2022-10-03 14:24:03 +0530

    Initial commit- added basic packaging script & pipeline defn (#1)
    Signed-off-by: Vikram Karve <vikram.karve@progress.com>

---
+++ creator/create_universal_pkg.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -v
+
+echo "${EXPEDITOR_CHANNEL}"
+echo "${EXPEDITOR_VERSION}"
+echo "${EXPEDITOR_PRODUCT_NAME}"
+echo "${EXPEDITOR_PRODUCT_KEY}"
+
+mkdir stage
+
+pushd stage
+
+curl -O https://packages.chef.io/files/current/chef/18.0.92/mac_os_x/11/chef-18.0.92-1.x86_64.dmg
+hdiutil attach -quiet chef-18.0.92-1.x86_64.dmg
+cp /Volumes/Chef\ Infra\ Client/chef-18.0.92-1.x86_64.pkg ./
+hdiutil detach -quiet /Volumes/Chef\ Infra\ Client
+rm chef-18.0.92-1.x86_64.dmg
+
+curl -O https://packages.chef.io/files/current/chef/18.0.92/mac_os_x/11/chef-18.0.92-1.arm64.dmg
+hdiutil attach -quiet chef-18.0.92-1.arm64.dmg
+cp /Volumes/Chef\ Infra\ Client/chef-18.0.92-1.arm64.pkg ./
+hdiutil detach -quiet /Volumes/Chef\ Infra\ Client
+rm chef-18.0.92-1.arm64.dmg
+
+popd
