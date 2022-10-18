97a7fb7f89aba691f7bf9e24cb1da6f995149254
Vikram Karve <vikram.karve@progress.com>
2022-10-18 15:40:15 +0530

    turn off cache flags for troubleshooting
    Signed-off-by: Vikram Karve <vikram.karve@progress.com>

--- omnibus/omnibus.rb
+++ omnibus/omnibus.rb
@@ -26,14 +26,14 @@
 
 # Disable git caching
 # ------------------------------
-use_git_caching true
+use_git_caching false
 
 # Enable S3 asset caching
 # ------------------------------
-use_s3_caching true
-s3_access_key  ENV["AWS_ACCESS_KEY_ID"]
-s3_secret_key  ENV["AWS_SECRET_ACCESS_KEY"]
-s3_bucket      "opscode-omnibus-cache"
+use_s3_caching false
+# s3_access_key  ENV["AWS_ACCESS_KEY_ID"]
+# s3_secret_key  ENV["AWS_SECRET_ACCESS_KEY"]
+# s3_bucket      "opscode-omnibus-cache"
 # s3_profile       ENV['AWS_S3_PROFILE']
 # s3_iam_role_arn  ENV['S3_IAM_ROLE_ARN']
 
