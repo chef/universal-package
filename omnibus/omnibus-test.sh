28449557066a786269641333e3d93334e9a627f9
Vikram Karve <vikram.karve@progress.com>
2022-11-02 19:53:39 +0530

    update install test
    Signed-off-by: Vikram Karve <vikram.karve@progress.com>

--- omnibus/omnibus-test.sh
+++ omnibus/omnibus-test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "--- Ensure 'chef' command works"
-chef -v
-
 if [ -d "/opt/chef" ]; then
   echo "Chef client installation directory exists."
 else
   echo "Chef client installation directory does not exist."
   exit 1
 fi
+
+echo "--- Ensure 'chef-client' command works"
+/opt/chef/bin/chef-client --version
