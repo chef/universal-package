4735485645cbaca7505c0b32bb1fe482967fa509
Kanad Kumar <kankumar@progress.com>
2022-11-23 19:40:06 +0530

    Added test commend
    Signed-off-by: Kanad Kumar <kankumar@progress.com>

--- omnibus/omnibus-test.sh
+++ omnibus/omnibus-test.sh
@@ -9,3 +9,18 @@ fi
 
 echo "--- Ensure 'chef-client' command works"
 /opt/chef/bin/chef-client --version
+
+FORK_OWNER="chef"
+REPO_NAME="chef.git"
+TAG_NAME="v18.0.185"
+echo "--- Getting $FORK_OWNER/$REPO_NAME repository cloning ---"
+# git clone -b [tag_name] [repository_url]
+git clone -b  $TAG_NAME https://github.com/$FORK_OWNER/$REPO_NAME
+
+cd chef
+source ../chef/omnibus/omnibus-test.sh
+exec "$@"
+
+
+
+
